65ae652af71c7fdf64085c6a60b89f3a972ec318
LissethPacheco <liszethth65@hotmail.com>
2025-05-27 21:26:11 -0500

    4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const servicios = {
   railway: "https://spectacular-mercy-production-f5b2.up.railway.app/",
   vercel: "https://vercel-ejemplo.vercel.app/",
-  heroku: "https://herokulis-0a22c78650c7.herokuapp.com/",
-  digitalocean: "https://practice-app-i8b6c.ondigitalocean.app/",
+  heroku: "https://heroku-proyect-dee4021d6db2.herokuapp.com/",
+  digitalocean: "http://147.182.191.27/",
   openai: "https://platform.openai.com/",
   gmail: "https://mail.google.com/"
 };
